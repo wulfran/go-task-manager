eecebbb26ec67091c2ec8649458f969d50497f5b
Piotr Nikonowicz <piotr.nikonowicz@gmail.com>
2026-02-10 12:07:41 +0100

    adjustments for userRepository_test.go

--- internal/db/queries/task/GetTask.sql
+++ internal/db/queries/task/GetTask.sql
@@ -1,3 +1,3 @@
-select name, priority, description, due_date,created_at, created_by
+select id, name, priority, description, due_date,created_at, created_by
 from tasks
 where id = $1
--- internal/repository/userRepository_test.go
+++ internal/repository/userRepository_test.go
@@ -2,13 +2,16 @@ package repository
 
 import (
 	"context"
+	"task-manager/internal/db"
 	"task-manager/internal/helpers"
 	"task-manager/internal/models"
 	"testing"
 )
 
-func testCreateUser(t *testing.T, ur UserRepository) {
+func testCreateUser(t *testing.T, db db.DB) {
 	t.Helper()
+
+	ur := NewUserRepository(db)
 
 	ctx := context.Background()
 	hash, _ := helpers.HashPassword("secretPassword")
@@ -26,10 +29,11 @@ func TestUserRepository_CreateUser(t *testing.T) {
 	hash, _ := helpers.HashPassword("secretPassword")
 
 	var tests = []struct {
-		name         string
-		payload      models.CreateUserPayload
-		expectsError bool
-		errorWanted  string
+		name                    string
+		payload                 models.CreateUserPayload
+		shouldCreateDefaultUser bool
+		expectsError            bool
+		errorWanted             string
 	}{
 		{
 			"valid payload",
@@ -39,15 +43,17 @@ func TestUserRepository_CreateUser(t *testing.T) {
 				Password: hash,
 			},
 			false,
+			false,
 			"",
 		},
 		{
-			"existing email, should error",
+			"existing email should error",
 			models.CreateUserPayload{
 				Name:     "Lorem Ipsum",
 				Email:    "lorem@ipsum.com",
 				Password: hash,
 			},
+			true,
 			true,
 			"CreateUser: failed to insert a new user: pq: duplicate key value violates unique constraint \"users_email_key\"",
 		},
@@ -55,21 +61,26 @@ func TestUserRepository_CreateUser(t *testing.T) {
 
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
+			t.Cleanup(func() {
+				_, _ = testDB.Exec("TRUNCATE users RESTART IDENTITY CASCADE ")
+			})
 			ctx := context.Background()
+			if tc.shouldCreateDefaultUser {
+				testCreateUser(t, *testDB)
+			}
 			err := userRepo.CreateUser(ctx, tc.payload)
-
 			if tc.expectsError {
 				if err == nil {
-					t.Error("createUser should return an error but it did not")
+					t.Error("function should return an error but it did not")
 				}
 
-				if tc.errorWanted != err.Error() {
+				if err != nil && tc.errorWanted != err.Error() {
 					t.Errorf("wrong error returned, expected <%s> but got <%s>", tc.errorWanted, err)
 				}
-			}
-
-			if !tc.expectsError && err != nil {
-				t.Errorf("unexpected error: %s", err)
+			} else {
+				if err != nil {
+					t.Errorf("unexpected error: %s", err)
+				}
 			}
 		})
 	}
@@ -77,26 +88,36 @@ func TestUserRepository_CreateUser(t *testing.T) {
 
 func TestUserRepository_CheckIfEmailExists(t *testing.T) {
 	userRepo := NewUserRepository(*testDB)
-	testCreateUser(t, userRepo)
+	testCreateUser(t, *testDB)
 	var tests = []struct {
-		name           string
-		email          string
-		expectedResult bool
+		name                    string
+		email                   string
+		shouldCreateDefaultUser bool
+		expectedResult          bool
 	}{
 		{
 			"no-existing e-mail, no error",
 			"john.doe@example.com",
+			false,
 			false,
 		},
 		{
 			"existing e-mail, no error",
 			"lorem@ipsum.com",
 			true,
+			true,
 		},
 	}
 
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
+			t.Cleanup(func() {
+				_, _ = testDB.Exec("TRUNCATE users RESTART IDENTITY CASCADE")
+			})
+			if tc.shouldCreateDefaultUser {
+				testCreateUser(t, *testDB)
+			}
+
 			result, err := userRepo.CheckIfEmailExists(tc.email)
 			if err != nil {
 				t.Errorf("unexpected error: %s", err)
@@ -111,7 +132,7 @@ func TestUserRepository_CheckIfEmailExists(t *testing.T) {
 
 func TestUserRepository_GetUserData(t *testing.T) {
 	userRepo := NewUserRepository(*testDB)
-	testCreateUser(t, userRepo)
+	testCreateUser(t, *testDB)
 	hash, _ := helpers.HashPassword("secretPassword")
 
 	var tests = []struct {
